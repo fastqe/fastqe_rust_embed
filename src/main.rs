6b073b25eb2054c6e0f2cf341e9a1fe1fee7e99f
lonsbio <andrew.lonsdale@lonsbio.com.au>
2024-11-28 22:54:08 +1100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-use rustpython_vm as vm;
+use rustpython_vm::{eval, Interpreter};
 
-fn main() -> vm::PyResult<()> {
-    vm::Interpreter::without_stdlib(Default::default()).enter(|vm| {
-        let scope = vm.new_scope_with_builtins();
-        let source = r#"print("Hello World!")"#;
-        let code_obj = vm
-            .compile(source, vm::compiler::Mode::Exec, "<embedded>".to_owned())
-            .map_err(|err| vm.new_syntax_error(&err, Some(source)))?;
-
-        vm.run_code_obj(code_obj, scope)?;
-
-        Ok(())
+pub unsafe extern "C" fn eval(s: *const u8, l: usize) -> u32 {
+    let src = std::slice::from_raw_parts(s, l);
+    let src = std::str::from_utf8(src).unwrap();
+    Interpreter::without_stdlib(Default::default()).enter(|vm| {
+        let res = eval::eval(vm, src, vm.new_scope_with_builtins(), "<string>").unwrap();
+        res.try_into_value(vm).unwrap()
     })
 }
+
+fn getrandom_always_fail(_buf: &mut [u8]) -> Result<(), getrandom::Error> {
+    Err(getrandom::Error::UNSUPPORTED)
+}
+
+getrandom::register_custom_getrandom!(getrandom_always_fail);
